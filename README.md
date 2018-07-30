2d0f0631e50d5011db62c0a52baa540fc20347fc
Ksalazar <kalfaros91@gmail.com>
2018-07-29 20:30:22 -0600

    complete app

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # liri
 liri-node-app like siri but text
+
+ LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface.
+ LIRI will be a command line node app that takes in parameters and gives you back data.
+    
+    You need  npm install --save prompt-list this is use to select one of the following options
+       1) 'my-tweets',
+       2) 'spotiy-this-song',
+       3) 'movie-this',
+       4) 'do-what-it-says'
+
+    which will do the following options
+
+    1) LIRI will display your latest tweets and the date it was created. You need:
+            npm install twitter and twitter developer credentials
+
+
+    2) LIRI will diplay a song from spotify api. To prompt the user for a song you will need to install npm install --save prompts then it will show the following information about the song
+        
+            * Artist(s)
+            * The song's name
+            * A preview link of the song from Spotify
+            * The album that the song is from
+    
+        If no song is provided then your program will default to "The Sign" by Ace of Base. You need:
+            npm install spotify-web-api-node --save and spotify developer credientials + token.
+    
+    3) LIRI will display a movie from the OMDB api. it will show the following information about the movie
+
+            * Title of the movie.
+            * Year the movie came out.
+            * IMDB Rating of the movie.
+            * Rotten Tomatoes Rating of the movie.
+            * Country where the movie was produced.
+            * Language of the movie.
+            * Plot of the movie.
+            * Actors in the movie.
+
+        If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.' You need:
+             npm install request
+
+    4) Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+    Additionally, you will need npm install dotenv to work with you .env file in order to get access to twitter and spotify. 
+
+
+   
---
+++ liri.js
@@ -0,0 +1,278 @@
+//require node modules
+var dotenv = require("dotenv").config();
+
+var Twitter = require('twitter');
+var fs = require("fs");
+
+var request = require('request');
+const  inquirer = require('inquirer');
+var song;
+
+
+//exports twitter and spotify keys
+var keys = require("./keys.js");
+
+//acces key information 
+var client = new Twitter(keys.twitter);
+
+
+
+
+function ask(){ 
+    var List = require('prompt-list');
+    var list = new List({
+    name: 'order',
+    message: 'What would you like to order?',
+    // choices may be defined as an array or a function that returns an array
+    choices: [
+        'my-tweets',
+        'spotiy-this-song',
+        'movie-this',
+        'do-what-it-says'
+    ]
+    });
+   
+    list.ask(function(answer) {
+       if(answer === 'my-tweets'){
+        tweets();
+       }
+       else if(answer === 'spotiy-this-song'){
+        music(1);
+       }
+       if(answer === 'movie-this'){
+        movie();
+       }
+       if(answer === 'do-what-it-says'){
+        file();
+       }
+    });
+
+}
+
+ask();
+
+function tweets(){
+
+    var params = {screen_name: '@karensa27136283', count:20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        //console.log(tweets);
+    }
+    
+    console.log(" ");
+    console.log("------------------");
+    console.log("-     TWEETS    -");
+    console.log("------------------");
+    for (var i = 0; i < tweets.length; i++){
+        var tweet = tweets[i].text;
+        var date = tweets[i].created_at;
+
+        console.log(">>>content: ");
+        console.log(" ");
+        console.log(tweet);
+        console.log(" ");
+        console.log(">>>Created it on: " + date);
+        console.log("****************************************************************");
+    }
+    //console.log(tweets);
+});
+
+
+}
+
+function music(num){
+
+    var Spotify = require('node-spotify-api');
+    
+    var spotify = new Spotify(keys.spotify);
+
+    if(num === 1){
+    
+        var Prompt = require('prompt-base');
+        var prompt = new Prompt({
+        name: 'song',
+        message: 'What song do you want to search?'
+        });
+        
+        // promise
+        prompt.run()
+        .then(function(answer) {
+
+            if(answer === undefined){ 
+                spotify.request("https://api.spotify.com/v1/tracks/5ksRONqssB7BR161NTtJAm" )
+                .then(function(data) {
+                    
+                    console.log(" ");
+                    console.log("-------------------");
+                    console.log("    Spotify Song");
+                    console.log("-------------------");
+                    console.log(" ");
+                    console.log(">>> Artist: " + data[i].artists[0].name);
+                    console.log(">>> Song: " + data[i].name);
+                    console.log(">>> Album: " + data[i].album.name);
+                    console.log(">>> Preview Url: " + data[i].preview_url);
+                })
+                .catch(function(err) {
+                console.error('Error occurred: ' + err); 
+                });
+
+            }
+            else{
+                spotify.search({ type: 'track', query: answer }, function(err, data) {
+                    if (err) {
+                    return console.log('Error occurred: ' + err);
+                    }
+                    var songinfo = data.tracks.items;
+                    for(var i =0; i<songinfo.artists.length; i++){
+                        console.log(" ");
+                        console.log("-------------------");
+                        console.log("    Spotify Song");
+                        console.log("-------------------");
+                        console.log(" ");
+                        console.log(">>> Artist: " + songinfo[i].artists[0].name);
+                        console.log(">>> Song: " + songinfo[i].name);
+                        console.log(">>> Album: " + songinfo[i].album.name);
+                        console.log(">>> Preview Url: " + songinfo[i].preview_url);
+                        console.log("****************************************************************");
+                    }      
+                    
+                
+                });
+        
+            }
+            
+        });
+    }
+    else{
+        console.log(song);
+        spotify.search({ type: 'track', query: song }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var songinfo = data.tracks.items;
+            for(var i =0; i<songinfo.artists.length; i++){
+                console.log(" ");
+                console.log("-------------------");
+                console.log("    Spotify Song");
+                console.log("-------------------");
+                console.log(" ");
+                console.log(">>> Artist: " + songinfo[i].artists[0].name);
+                console.log(">>> Song: " + songinfo[i].name);
+                console.log(">>> Album: " + songinfo[i].album.name);
+                console.log(">>> Preview Url: " + songinfo[i].preview_url);
+                console.log("****************************************************************");
+            }      
+    
+        });
+
+    }
+      
+}
+
+function movie(){ 
+    var Prompt = require('prompt-base');
+    var prompt = new Prompt({
+      name: 'song',
+      message: 'What song do you want to search?'
+    });
+     
+    // promise
+    prompt.run()
+      .then(function(answer) {
+
+        if(answer === undefined){ 
+            request("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy", function(error, response, body) {
+                
+                if (!error && response.statusCode === 200) {
+                    var movie = JSON.parse(body); 
+                    console.log(" ");
+                    console.log("-------------------");
+                    console.log("       Movie");
+                    console.log("-------------------");
+                    console.log(" ");
+                    console.log(">>> Title: " + movie.Title);
+                    console.log(">>> Year: " + movie.Year);
+                    console.log(">>> Imdb Rating: " + movie.imdbRating);
+                    console.log(">>> Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+                    console.log(">>> Country: " + movie.Country);
+                    console.log(">>> Language: " + movie.Language);
+                    console.log(">>> Plot: " + movie.Plot);
+                    console.log(">>> Actors: " + movie.Actors);
+                }
+                else{
+                    console.log('error:', error);
+                }
+            });
+
+        }
+        else{
+            var x = answer.split(" ");
+            var movieName = "";
+            for(var i=0; i<x.length; i++){
+                if(i != x.length-1){
+                    movieName += x[i] + "+";
+                }
+                else{
+                    movieName += x[i]
+                }
+                
+            }
+
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+            // This line is just to help us debug against the actual URL.
+            console.log(queryUrl);
+
+            request(queryUrl, function(error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+
+                    var movie = JSON.parse(body); 
+                    console.log(" ");
+                    console.log("-------------------");
+                    console.log("       Movie");
+                    console.log("-------------------");
+                    console.log(" ");
+                    console.log(">>> Title: " + movie.Title);
+                    console.log(">>> Year: " + movie.Year);
+                    console.log(">>> Imdb Rating: " + movie.imdbRating);
+                    console.log(">>> Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+                    console.log(">>> Country: " + movie.Country);
+                    console.log(">>> Language: " + movie.Language);
+                    console.log(">>> Plot: " + movie.Plot);
+                    console.log(">>> Actors: " + movie.Actors);
+                }
+                else{
+                    console.log('error:', error);
+                }
+            });
+              
+            
+        }
+          
+        
+       
+    
+          
+      })
+      
+
+}
+
+function file(){ 
+
+    fs.readFile("random.txt", "utf8",  function(err, data){
+        if(err){
+            console.log(err);
+        }
+
+        song = data;
+        music(0);
+
+    });
+
+}
+
+
+
+ 
